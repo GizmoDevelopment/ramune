c2628c93b12dad6f00a07e7ac1b2046ce8a1957a
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-18 15:11:26 +0200

    vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import svgLoader from "vite-svg-loader";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		svgLoader()
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 			"@typings": path.resolve(__dirname, "./src/typings"),
 			"@views": path.resolve(__dirname, "./src/views"),
 			"@components": path.resolve(__dirname, "./src/components"),
-			"@utils": path.resolve(__dirname, "./src/utils")
+			"@utils": path.resolve(__dirname, "./src/utils"),
+			"@assets": path.resolve(__dirname, "./src/assets")
 		}
 	}
 });
-

8ac438bddbd31dce5d0dc356591f73b6a20ef745
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-19 14:55:38 +0200

    basic episode card

--- src/components/ShowEpisodeCard.vue
+++ src/components/ShowEpisodeCard.vue
@@ -1,7 +1,13 @@
 <template>
-	<div class="episode-container">
-		<p>{{ episode.title }}</p>
-	</div>
+	<router-link
+		to="/test"
+		class="anchor-without-decoration"
+	>
+		<div class="episode-container hover-container">
+			<img class="episode-thumbnail" :src="episode.thumbnail_url">
+			<p class="episode-title">{{ episode.title }}</p>
+		</div>
+	</router-link>
 </template>
 
 <script lang="ts">
@@ -23,3 +29,28 @@
 	});
 
 </script>
+
+<style scoped>
+
+	.episode-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+		width: 200px;
+		padding: 4px;
+	}
+	.episode-thumbnail {
+		width: 100%;
+		height: auto;
+		border-radius: var(--card-border-radius);
+	}
+
+	.episode-title {
+		margin-top: 5px;
+		width: calc(100% - 4px);
+		text-align: left;
+		min-height: 40px;
+	}
+
+</style>

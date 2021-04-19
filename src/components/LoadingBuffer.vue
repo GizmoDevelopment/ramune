32e67fe33ae5ad27b21fb06050f40e855a86b794
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-19 12:43:08 +0200

    animated loading buffer component

---
+++ src/components/LoadingBuffer.vue
@@ -0,0 +1,42 @@
+<template>
+	<Logo class="logo" />
+</template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	// Icons
+	import Logo from "@assets/icons/logo.svg";
+
+	export default defineComponent({
+		name: "LoadingBuffer",
+		components: {
+			Logo
+		}
+	});
+
+</script>
+
+<style scoped>
+
+	@keyframes loading {
+		0% {
+			transform: scale3d(1, .2, 1);
+		}
+		50% {
+			transform: scale3d(1, 1.4, 1) translateY(-150px);
+		}
+		100% {
+			transform: scale3d(1, .2, 1) translateY(0px);
+		}
+	}
+
+	.logo {
+		width: auto;
+		height: 5em;
+		animation: loading 2s ease-in-out infinite;
+	}
+
+</style>

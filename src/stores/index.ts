01b181dd322caa122656a6d979504ce374c1bfd6
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-09 13:04:37 +0200

    store user object

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,18 +2,23 @@
 import { createStore } from "vuex";
 
 // Types
+import { AuthenticatedUser } from "gizmo-api/lib/types";
 import { Show } from "@typings/types";
 import { State } from "@typings/vuex";
 
 export default createStore<State>({
 	state () {
 		return {
-			shows: new Map()
+			shows: new Map(),
+			user: null
 		};
 	},
 	mutations: {
 		CACHE_SHOW (state: State, show: Show) {
 			state.shows.set(show.id, show);
+		},
+		UPDATE_USER (state: State, user: AuthenticatedUser) {
+			state.user = user;
 		}
 	}
 });
--- src/typings/vuex.ts
+++ src/typings/vuex.ts
@@ -2,16 +2,19 @@
 import { Store } from "vuex";
 
 // Types
+import { AuthenticatedUser } from "gizmo-api/lib/types";
 import { Show } from "@typings/types";
 
 export interface State {
     shows: Map<string, Show>;
+	user: AuthenticatedUser | null;
 }
 
 declare module "@vue/runtime-core" {
 
     export interface State {
         shows: Map<string, Show>;
+		user: AuthenticatedUser | null;
     }
 
     interface ComponentCustomProperties {

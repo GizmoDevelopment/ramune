4dbcbfaf5112677f4bb1920afffdae294bd8ea6c
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-10 12:19:55 +0200

    room list

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,5 +1,20 @@
 <template>
-	<h1 class="heading">Rooms</h1>
+	<div>
+		<h1 class="heading">Rooms</h1>
+		<div v-if="user">
+			<div v-if="rooms">
+				<div v-for="room in rooms" :key="room.id">
+					<RoomCard :room="room" />
+				</div>
+			</div>
+			<div v-else>
+				<LoadingBuffer />
+			</div>
+		</div>
+		<div v-else>
+			<Error text="You must be logged in to view this page" />
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -7,8 +22,30 @@
 	// Modules
 	import { defineComponent } from "vue";
 
+	// Components
+	import Error from "@components/Error.vue";
+	import LoadingBuffer from "@components/LoadingBuffer.vue";
+	import RoomCard from "@components/RoomCard.vue";
+
+	// Types
+	import { Room } from "@typings/room";
+
 	export default defineComponent({
-		name: "Rooms"
+		name: "Rooms",
+		components: {
+			Error,
+			LoadingBuffer
+		},
+		data () {
+			return {
+				rooms: null as Room[] | null
+			};
+		},
+		computed: {
+			user () {
+				return this.$store.state.user;
+			}
+		}
 	});
 
 </script>

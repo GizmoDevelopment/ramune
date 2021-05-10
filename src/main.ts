5e0bc2f81501965c07e230d7126e362b5cc6c7ec
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-10 12:41:12 +0200

    socket.io

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 // Modules
 import { createApp } from "vue";
+import VueSocketIO from "vue-socket.io";
+import SocketIO from "socket.io-client";
 
 // Vue
 import App from "./App.vue";
@@ -10,7 +12,30 @@ import Router from "./routes";
 // Stores
 import Store from "./stores";
 
-createApp(App)
-	.use(Router)
-	.use(Store)
-	.mount("#app");
+// Constants
+const SOCKET_ENDPOINT = import.meta.env.VITE_SOCKET_ENDPOINT;
+
+/**
+ * !!! NOTE !!!
+ * REMOVE PATCH-PACKAGE ONCE VUE-SOCKET.IO OFFICIALLY SUPPORTS VUE 3
+ */
+
+if (typeof SOCKET_ENDPOINT === "string") {
+
+	createApp(App)
+		.use(Router)
+		.use(Store)
+		.use(new VueSocketIO({
+			debug: true,
+			connection: SocketIO(SOCKET_ENDPOINT),
+			vuex: {
+				store: Store,
+				actionPrefix: "SOCKET_",
+				mutationPrefix: "SOCKET_",
+			}
+		}))
+		.mount("#app");
+
+} else {
+	throw new Error("Environmental variable 'SOCKET_ENDPOINT' is not assigned");
+}

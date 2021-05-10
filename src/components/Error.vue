d7185e8025823324598338b009fbfd951be3be97
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-10 12:09:54 +0200

    error component

---
+++ src/components/Error.vue
@@ -0,0 +1,20 @@
+<template>
+	<h3 class="opaque-label">{{ text }}</h3>
+</template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "Error",
+		props: {
+			text: {
+				type: String,
+				required: true
+			}
+		}
+	});
+
+</script>

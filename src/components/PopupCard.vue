5c39c55dbc68a534982a75032434b11b53a081f8
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-11 14:06:56 +0200

    popup transitions

--- src/components/PopupCard.vue
+++ src/components/PopupCard.vue
@@ -1,14 +1,29 @@
 <template>
-	<div class="overlay" @click="$emit('dismiss')">
-		<div class="container" @click.stop>
-			<div class="title-bar">
-				<Close
-					class="icon"
-					@click="$emit('dismiss')"
-				/>
+	<transition name="fade-popup">
+		<div
+			v-if="show"
+			class="overlay"
+		/>
+	</transition>
+	<div
+		class="overlay-container"
+		@click="$emit('dismiss')"
+	>
+		<transition name="fade-and-slide-popup" mode="out-in">
+			<div
+				v-if="show"
+				class="container"
+				@click.stop
+			>
+				<div class="title-bar">
+					<Close
+						class="icon"
+						@click="$emit('dismiss')"
+					/>
+				</div>
+				<slot class="content" />
 			</div>
-			<slot class="content" />
-		</div>
+		</transition>
 	</div>
 </template>
 
@@ -25,24 +40,62 @@
 		components: {
 			Close
 		},
-		emits: [ "dismiss" ]
+		emits: [ "dismiss" ],
+		data () {
+			return {
+				show: false
+			};
+		},
+		mounted () {
+			this.show = true;
+		},
+		beforeUnmount () {
+			this.show = false;
+		}
 	});
 
 </script>
 
 <style scoped>
 
-	.overlay {
+	.fade-popup-enter-active,
+	.fade-popup-leave-active {
+		transition: all .5s ease;
+	}
+
+	.fade-popup-enter-from,
+	.fade-popup-leave-to {
+		opacity: 0;
+	}
+
+	.fade-and-slide-popup-enter-active,
+	.fade-and-slide-popup-leave-active {
+		transition: all .4s ease;
+	}
+
+	.fade-and-slide-popup-enter-from,
+	.fade-and-slide-popup-leave-to {
+		transform: translateY(50px);
+		opacity: 0;
+	}
+
+	.overlay, .overlay-container {
 		position: absolute;
 		width: 100%;
 		height: 100%;
 		top: 0;
 		left: 0;
+	}
+
+	.overlay {
+		background: rgba(0, 0, 0, .5);
+	}
+
+	.overlay-container {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		align-content: center;
-		background: rgba(0, 0, 0, .5);
 	}
 
 	.container {
@@ -86,11 +139,11 @@
 
 	/*
 	
-		.slide-popup-enter-active, .slide-popup-leave-active {
+		.fade-and-slide-popup-enter-active, .fade-and-slide-popup-leave-active {
 		transition: all .2s ease;
 	}
 
-	.slide-popup-enter-from, .slide-popup-leave-to {
+	.fade-and-slide-popup-enter-from, .fade-and-slide-popup-leave-to {
 		opacity: 0;
 		transform: translateY(50px);
 	}

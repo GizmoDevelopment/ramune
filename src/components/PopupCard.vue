e348e241b669df922c3424ca7ee66d8a90ea355c
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-11 10:29:27 +0200

    popup component

---
+++ src/components/PopupCard.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="overlay" @click="$emit('dismiss')">
+		<div class="container" @click.stop>
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "PopupCard",
+		emits: [ "dismiss" ]
+	});
+
+</script>
+
+<style scoped>
+
+	.overlay {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		align-content: center;
+		background: rgba(0, 0, 0, .5);
+	}
+
+	.container {
+		position: relative;
+		background-color: var(--container-background-color);
+		border-radius: var(--popup-border-radius);
+		padding-top: 5px;
+		padding-bottom: 5px;
+		padding-left: 25px;
+		padding-right: 25px;
+	}
+
+	/*
+	
+		.slide-popup-enter-active, .slide-popup-leave-active {
+		transition: all .2s ease;
+	}
+
+	.slide-popup-enter-from, .slide-popup-leave-to {
+		opacity: 0;
+		transform: translateY(50px);
+	}
+	*/
+
+</style>

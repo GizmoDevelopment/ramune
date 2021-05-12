05a0f49faf8d3c6d971d3e095466ad598a6b0b46
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-12 08:36:07 +0200

    add overlay to ShowEpisodeCard

--- src/components/ShowEpisodeCard.vue
+++ src/components/ShowEpisodeCard.vue
@@ -1,13 +1,17 @@
 <template>
-	<router-link
-		to="/test"
-		class="anchor-without-decoration"
-	>
-		<div class="episode-container hover-container">
+	<div class="episode-container">
+		<div class="thumbnail-container">
 			<img class="episode-thumbnail" :src="episode.thumbnail_url">
-			<p class="episode-title">{{ episode.title }}</p>
+			<div class="episode-info">
+				<div class="episode-index">{{ episode.id }}</div>
+				<div class="episode-duration">24:00</div>
+			</div>
+			<div class="overlay">
+				<Play class="overlay-play-icon" />
+			</div>
 		</div>
-	</router-link>
+		<p class="episode-title">{{ episode.title }}</p>
+	</div>
 </template>
 
 <script lang="ts">
@@ -15,11 +19,17 @@
 	// Modules
 	import { defineComponent, PropType } from "vue";
 
+	// Icons
+	import Play from "@assets/icons/play.svg";
+
 	// Types
 	import { Episode } from "@typings/show";
 
 	export default defineComponent({
 		name: "ShowEpisodeCard",
+		components: {
+			Play
+		},
 		props: {
 			episode: {
 				type: Object as PropType<Episode>,
@@ -40,11 +50,70 @@
 		width: 200px;
 		padding: 4px;
 	}
-	
-	.episode-thumbnail {
+
+	.thumbnail-container, .overlay {
+		position: relative;
+		display: block;
+		height: 7em;
+	}
+
+	.overlay {
+		position: absolute;
 		width: 100%;
-		height: 112.5px;
+		height: 100%;
+		left: 0;
+		top: 0;
+		background-color: var(--overlay-background-color);
+		opacity: 0;
+		transition: opacity .2s ease;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+	
+	.overlay:hover {
+		opacity: 1;
+		cursor: pointer;
+	}
+
+	.episode-thumbnail {
+		height: inherit;
+	}
+
+	.episode-thumbnail, .overlay {
 		border-radius: var(--card-border-radius);
+	}
+
+	.overlay-container, .episode-info {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	.overlay-play-icon {
+		font-size: 4em;
+	}
+
+	.episode-info {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: flex-end;
+		
+	}
+
+	.episode-info * {
+		background-color: var(--overlay-background-color);
+		border-radius: var(--card-border-radius);
+		margin-left: 5px;
+		margin-right: 5px;
+		margin-bottom: 5px;
+		padding-left: .25em;
+		padding-right: .25em;
+		font-weight: 400;
 	}
 
 	.episode-title {

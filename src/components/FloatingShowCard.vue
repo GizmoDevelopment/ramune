df33a0fef7cb7e132ecc64d8b1811306802cc7cf
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-12 12:04:09 +0200

    refactor show information into its own component

--- src/components/FloatingShowCard.vue
+++ src/components/FloatingShowCard.vue
@@ -17,25 +17,9 @@
 					<Close class="popup-close-button" @click="$emit('dismiss')" />
 				</div>
 				<div v-if="show">
-					<div class="show-information">
-						<img :src="show.poster_url" class="show-artwork">
-						<div class="show-details">
-							<h1 class="heading show-title">{{ show.title }}</h1>
-							<div class="show-stats">
-								<ShowScoreLabel :score="show.score" class="show-score" />
-								<div class="show-episode-count">{{ episodeCount }} Episodes</div>
-							</div>
-							<p class="show-description">{{ formattedDescription }}</p>
-						</div>
-					</div>
-					<div class="season-container">
-						<div v-for="season in show.seasons" :key="season.title">
-							<ShowSeasonEpisodeList
-								:season="season"
-								@play-episode="playEpisode"
-							/>
-						</div>
-					</div>
+					<ShowInformation
+						:show="show"
+					/>
 				</div>
 				<div v-else-if="status">
 					<div v-if="status === 'loading'">
@@ -56,25 +40,23 @@
 	import { defineComponent, PropType } from "vue";
 
 	// Components
-	import ShowScoreLabel from "@components/ShowScoreLabel.vue";
-	import ShowSeasonEpisodeList from "@components/ShowSeasonEpisodeList.vue";
 	import Error from "@components/Error.vue";
 	import LoadingBuffer from "@components/LoadingBuffer.vue";
+	import ShowInformation from "@components/ShowInformation.vue";
 
 	// Icons
 	import Close from "@assets/icons/close.svg";
 
 	// Types
-	import { Show, Season } from "@typings/show";
+	import { Show } from "@typings/show";
 
 	export default defineComponent({
 		name: "FloatingShowCard",
 		components: {
-			ShowScoreLabel,
-			ShowSeasonEpisodeList,
 			Close,
 			Error,
-			LoadingBuffer
+			LoadingBuffer,
+			ShowInformation
 		},
 		props: {
 			show: {
@@ -86,26 +68,7 @@
 				default: ""
 			}
 		},
-		emits: [ "dismiss" ],
-		computed: {
-			episodeCount (): number {
-				if (this.show) {
-					return this.show.seasons.reduce((total: number, curr: Season): number => {
-						return total + curr.episodes.length;
-					}, 0);
-				} else {
-					return 0;
-				}
-			},
-			formattedDescription (): string {
-				return this.show?.description?.replace(/\\n/g, "\n") || "";
-			}
-		},
-		methods: {
-			playEpisode (episodeId: number) {
-				this.$router.push(`/watch/${ this.show.id }/${ episodeId }`);
-			}
-		}
+		emits: [ "dismiss" ]
 	});
 
 </script>
@@ -185,60 +148,6 @@
 
 	.popup-close-button:hover {
 		cursor: pointer;
-	}
-
-	/* Show Information */
-
-
-	.show-information {
-		margin-top: 1em;
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-	}
-
-	.show-artwork {
-		border-radius: var(--card-border-radius);
-		height: 280px;
-		width: auto;
-	}
-
-	.show-details {
-		margin-left: 20px;
-	}
-
-	.show-title {
-		margin-top: .2em;
-		margin-bottom: 10px;
-	}
-
-	.show-stats {
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-		align-items: center;
-	}
-
-	.show-stats * {
-		margin-right: 10px;
-	}
-
-	.show-score {
-		background-color: var(--primary-color);
-		border: 2px solid var(--primary-color);
-		border-radius: 20px;
-		padding-left: 6px;
-		padding-right: 6px;
-	}
-
-	.show-description {
-		text-align: left;
-		font-size: 15px;
-		white-space: pre-line;
-	}
-
-	.season-container {
-		width: 100%;
 	}
 
 </style>

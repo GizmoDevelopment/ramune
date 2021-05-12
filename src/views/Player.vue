c16e08ae3415115bae611489f28320b48528cbb3
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-12 14:13:15 +0200

    initial player code

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,6 +1,14 @@
 <template>
-	Show {{ showId }}
-	Episode {{ episodeId }}
+	<div v-if="show">
+		{{ show.title }}
+		{{ episodeId }}
+	</div>
+	<div v-else-if="status">
+		<Error :text="status" />
+	</div>
+	<div v-else>
+		<LoadingBuffer />
+	</div>
 </template>
 
 <script lang="ts">
@@ -8,8 +16,22 @@
 	// Modules
 	import { defineComponent } from "vue";
 
+	// Components
+	import LoadingBuffer from "@components/LoadingBuffer.vue";
+	import Error from "@components/Error.vue";
+
+	// Utils
+	import { getShow } from "@utils/api";
+
+	// Types
+	import { Show } from "@typings/show";
+
 	export default defineComponent({
 		name: "Player",
+		components: {
+			LoadingBuffer,
+			Error
+		},
 		props: {
 			showId: {
 				type: String,
@@ -19,6 +41,39 @@
 				type: Number,
 				default: 1
 			}
+		},
+		data () {
+			return {
+				show: null as Show | null,
+				status: "" as string | number
+			};
+		},
+		async mounted () {
+
+			const cachedShows = this.$store.state.shows;
+
+			if (cachedShows.has(this.showId)) {
+				
+				const _show = cachedShows.get(this.showId);
+
+				if (_show) {
+					this.show = _show;
+				} else {
+					this.status = 404;
+				}
+
+			} else {
+
+				const _show = await getShow(this.showId);
+
+				if (_show) {
+					this.show = _show;
+				} else {
+					this.status = 404;
+				}
+
+			}
+
 		}
 	});
 

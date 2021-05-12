afdf0f7480effc17b28150a332f049932d607b76
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-12 11:44:13 +0200

    allow status codes on Error component

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,5 +1,5 @@
 <template>
-	<h3 class="opaque-label">{{ text }}</h3>
+	<h3 class="opaque-label">{{ label }}</h3>
 </template>
 
 <script lang="ts">
@@ -12,7 +12,25 @@
 		props: {
 			text: {
 				type: String,
-				required: true
+				default: "Something went wrong"
+			}
+		},
+		data () {
+			return {
+				label: "" as string
+			};
+		},
+		mounted () {
+			if (parseInt(this.text) > 0) {
+				switch (parseInt(this.text)) {
+					case 404:
+						this.label = "Not found :(";
+						break;
+					default:
+						this.label = this.text;
+				}
+			} else {
+				this.label = this.text;
 			}
 		}
 	});

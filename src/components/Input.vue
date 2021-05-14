7f4406d1378afcac647907f2f2d377e4ee5f18a6
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-14 12:25:17 +0200

    input element component

---
+++ src/components/Input.vue
@@ -0,0 +1,37 @@
+<template>
+	<input
+		:type="type"
+		:placeholder="placeholder"
+		@input="inputChange"
+	>
+</template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "Input",
+		props: {
+			type: {
+				type: String,
+				default: "text"
+			},
+			placeholder: {
+				type: String,
+				default: "Placeholder"
+			}
+		},
+		emits: [ "update" ],
+		methods: {
+			inputChange (e: Event) {
+
+				const target = <HTMLInputElement>e.target;
+
+				this.$emit("update", target.value);
+			}
+		}
+	});
+
+</script>

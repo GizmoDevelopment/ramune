bd76317ed990520911b66a403b93c2c614065b9a
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-14 10:54:20 +0200

    popup component

--- src/components/PopupCard.vue
+++ src/components/PopupCard.vue
@@ -1,30 +1,26 @@
 <template>
-	<transition name="fade-popup">
+	<transition name="fade-overlay">
 		<div
-			v-if="show"
+			v-if="visible"
 			class="overlay"
+			@click="$emit('dismiss')"
 		/>
 	</transition>
-	<div
-		class="overlay-container"
-		@click="$emit('dismiss')"
-	>
-		<transition name="fade-and-slide-popup" mode="out-in">
-			<div
-				v-if="show"
-				class="container"
-				@click.stop
-			>
-				<div class="title-bar">
-					<Close
-						class="icon"
-						@click="$emit('dismiss')"
-					/>
+	<transition name="slide-content">
+		<div
+			v-if="visible"
+			id="content-container"
+			@click="$emit('dismiss')"
+		>
+			<div id="content" @click.stop>
+				<div id="popup-title-bar">
+					<h2>{{ title }}</h2>
+					<Close class="popup-close-button" @click="$emit('dismiss')" />
 				</div>
-				<slot class="content" />
+				<slot />
 			</div>
-		</transition>
-	</div>
+		</div>
+	</transition>
 </template>
 
 <script lang="ts">
@@ -40,113 +36,100 @@
 		components: {
 			Close
 		},
-		emits: [ "dismiss" ],
-		data () {
-			return {
-				show: false
-			};
+		props: {
+			title: {
+				type: String,
+				required: true
+			},
+			visible: {
+				type: Boolean,
+				default: false
+			}
 		},
-		mounted () {
-			this.show = true;
-		},
-		beforeUnmount () {
-			this.show = false;
-		}
+		emits: [ "dismiss" ]
 	});
 
 </script>
 
 <style scoped>
 
-	.fade-popup-enter-active,
-	.fade-popup-leave-active {
-		transition: all .5s ease;
+	.fade-overlay-enter-active,
+	.fade-overlay-leave-active {
+		transition: opacity .3s ease-in-out;
 	}
 
-	.fade-popup-enter-from,
-	.fade-popup-leave-to {
+	.fade-overlay-enter-from,
+	.fade-overlay-leave-to {
 		opacity: 0;
 	}
 
-	.fade-and-slide-popup-enter-active,
-	.fade-and-slide-popup-leave-active {
-		transition: all .4s ease;
+	.slide-content-enter-active,
+	.slide-content-leave-active {
+		transition: transform .3s ease, opacity .3s ease;
 	}
 
-	.fade-and-slide-popup-enter-from,
-	.fade-and-slide-popup-leave-to {
+	.slide-content-enter-from,
+	.slide-content-leave-to {
 		transform: translateY(50px);
 		opacity: 0;
 	}
 
-	.overlay, .overlay-container {
-		position: absolute;
+	.overlay, #content-container {
+		position: fixed;
+	}
+
+	#content-container {
 		width: 100%;
 		height: 100%;
 		top: 0;
 		left: 0;
 	}
 
-	.overlay {
-		background: rgba(0, 0, 0, .5);
-	}
-
-	.overlay-container {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		align-content: center;
-	}
-
-	.container {
-		position: relative;
-		background-color: var(--container-background-color);
-		border-radius: var(--popup-border-radius);
-		padding-top: 5px;
-		padding-bottom: 5px;
-		padding-left: 25px;
-		padding-right: 25px;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		overflow: hidden;
-	}
-
-	.title-bar {
-		width: calc(100% + 25px); /* Ignore padding to properly position X button */
+	#content-container {
 		display: flex;
 		flex-direction: row;
-		justify-content: flex-end;
+		justify-content: center;
+		align-items: flex-start;
+		overflow-y: auto;
 	}
 
-	.icon {
+	#content {
+		margin-top: 5%;
+		margin-bottom: 20px;
+		width: auto;
+		background-color: var(--container-background-color);
+		border-radius: var(--popup-border-radius);
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+		padding-top: 1em;
+		padding-bottom: 1em;
+		padding-left: 2em;
+		padding-right: 2em;
+	}
+
+	#popup-title-bar {
+		width: calc(100% + 1em); /* Push into padding so the close button sits properly in the corner */
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 2em;
+	}
+
+	#popup-title-bar h2 {
+		margin-top: 0;
+		margin-bottom: 0;
+		margin-right: 70px;
+	}
+
+	.popup-close-button {
 		height: 2em;
 	}
 
-	.icon:hover {
+	.popup-close-button:hover {
 		cursor: pointer;
 	}
-
-	@media only screen and (max-width: 780px) {
-
-		.container {
-			width: 100%;
-			height: 100%;
-			border-radius: 0;
-		}
-
-	}
-
-	/*
-	
-		.fade-and-slide-popup-enter-active, .fade-and-slide-popup-leave-active {
-		transition: all .2s ease;
-	}
-
-	.fade-and-slide-popup-enter-from, .fade-and-slide-popup-leave-to {
-		opacity: 0;
-		transform: translateY(50px);
-	}
-	*/
 
 </style>

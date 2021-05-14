da335d0b8c370f84d67f599d97a2b412910174b8
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-14 10:08:19 +0200

    button class

--- src/App.vue
+++ src/App.vue
@@ -140,6 +140,27 @@
 		text-align: left;
 	}
 
+	.button {
+		background-color: var(--primary-color);
+		color: var(--text-color);
+		border: 2px solid var(--primary-color);
+		border-radius: var(--popup-border-radius);
+		font-weight: bold;
+		font-size: 1.5em;
+		transition: background-color .2s ease-in-out, border-color .2s ease-in-out;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		align-content: center;
+		padding: 4px;
+	}
+
+	.button:hover {
+		background-color: var(--primary-hover-color);
+		border-color: var(--primary-hover-color);
+	}
+
 	.faded {
 		opacity: .5;
 	}

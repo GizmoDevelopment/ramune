caebc20ee9ceb8799840cd1bd7b70b39dcef3208
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-22 15:56:48 +0200

    room pop-out card

--- src/components/RoomPopout.vue
+++ src/components/RoomPopout.vue
@@ -1,6 +1,20 @@
 <template>
-	<div v-if="room" id="container">
-		room.name
+	<div v-if="room" id="position-container">
+		<div id="container">
+			<div id="room-information">
+				<RoomUserList
+					id="user-list"
+					:users="room.users"
+					:host="room.host"
+				/>
+			</div>
+			<div id="video">
+				<p class="faded">Nothing is currently playing</p>
+			</div>
+			<div id="overlay" class="overlay hover-opacity" @click="returnToRoom()">
+				<button class="primary-button">Return</button>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -9,14 +23,27 @@
 	// Modules
 	import { defineComponent } from "vue";
 
+	// Components
+	import RoomUserList from "@components/RoomUserList.vue";
+
 	// Types
 	import { Room } from "@typings/room";
 
 	export default defineComponent({
 		name: "RoomPopout",
+		components: {
+			RoomUserList
+		},
 		computed: {
 			room (): Room | null {
 				return this.$store.state.room;
+			}
+		},
+		methods: {
+			returnToRoom () {
+				if (this.room) {
+					this.$router.push(`/rooms/${ this.room.id }`);
+				}
 			}
 		}
 	});
@@ -25,10 +52,55 @@
 
 <style scoped>
 
-	#container {
-		position: absolute;
+	#position-container {
+		position: fixed;
 		right: 0;
 		bottom: 0;
+	}
+
+	#container {
+		position: relative;
+		background-color: var(--container-background-color);
+		border-radius: var(--popup-border-radius);
+		width: 400px;
+		height: calc(400px * 9/16);
+		margin-right: 15px;
+		margin-bottom: 15px;
+	}
+
+	#video {
+		position: absolute;
+		width: inherit;
+		top: 0;
+		left: 0;
+		height: inherit;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	#video p {
+		user-select: none;
+	}
+
+	#room-information {
+		width: 100%;
+		height: 100%;
+		padding: 5px;
+	}
+
+	#user-list {
+		height: 2em;
+	}
+
+	#overlay {
+		border-radius: var(--popup-border-radius);
+	}
+
+	#overlay button:hover {
+		background-color: var(--primary-color);
+		cursor: pointer;
 	}
 
 </style>

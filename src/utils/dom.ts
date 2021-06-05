34051d695dfac00a25a6890a436fa871fc1799d6
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-06-05 14:19:16 +0200

    setCookie method

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -11,3 +11,20 @@ export function changePageTitle (title: string): void {
 export function clearPageTitle (): void {
 	document.title = "Ramune";
 }
+
+export function getCookie (name: string): string | null {
+
+	const
+		cookies = document.cookie.split("; "),
+		cookieMatch = new RegExp(`^${name}=`);
+
+	const cookie = cookies.find((cookie: string) => cookie.match(cookieMatch));
+
+	return cookie
+		? decodeURIComponent(cookie.replace(`${name}=`, ""))
+		: null;
+}
+
+export function setCookie (name: string, value: string): void {
+	document.cookie = `${ name }=${ value }`;
+}

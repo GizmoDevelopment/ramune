cafc00152f8d7b8c8a32cb19acf5b0d29f41be9d
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-06-05 14:19:21 +0200

    move getCookie to dom utils

--- src/utils/essentials.ts
+++ src/utils/essentials.ts
@@ -4,16 +4,3 @@
 export function pluralize <T> (input: string, reference: T[]): string {
 	return input.replace(/%NUM%/gm, reference.length.toString()).replace(/%S%/gm, reference.length === 1 ? "" : "s");
 }
-
-export function getCookie (name: string): string | null {
-	
-	const
-		cookies = document.cookie.split("; "),
-		cookieMatch = new RegExp(`^${ name }=`);
-
-	const cookie = cookies.find((cookie: string) => cookie.match(cookieMatch));
-
-	return cookie
-		? decodeURIComponent(cookie.replace(`${ name }=`, ""))
-		: null;
-}

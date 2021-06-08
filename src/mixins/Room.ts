5f601d178b91378c0af645c6ca726ab127add3aa
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-06-08 19:43:22 +0200

    room mixin

---
+++ src/mixins/Room.ts
@@ -0,0 +1,43 @@
+// Modules
+import { defineComponent, PropType } from "vue";
+
+// Utils
+import { getEpisodeById } from "@utils/show";
+
+// Types
+import { AuthenticatedUser } from "gizmo-api/lib/types";
+import { Room } from "@typings/room";
+import { Episode, Show } from "@typings/show";
+
+export default defineComponent({
+	props: {
+		room: {
+			type: Object as PropType<Room>,
+			required: true
+		}
+	},
+	computed: {
+		user (): AuthenticatedUser | null {
+			return this.$store.state.user;
+		},
+		episode (): Episode | null {
+			return this.show
+				? getEpisodeById(this.show, this.episodeId)
+				: null;
+		},
+		episodeId (): number {
+			return this.room.data?.episodeId || 1;
+		},
+		show (): Show | null {
+			return this.room.data?.show || null;
+		},
+		isHost (): boolean {
+			return this.room.host.id === this.user?.id;
+		}
+	},
+	methods: {
+		toggleRoomControllerState (state: string | null) {
+			this.$store.commit("UPDATE_ROOM_CONTROLLER_STATE", state);
+		}
+	}
+});

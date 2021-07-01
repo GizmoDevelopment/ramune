65ae330f1215a9378e96bd7ce5dfb7c8e1fcc4a9
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-07-01 16:51:28 +0200

    Merge RoomVideo & RoomVideoController

--- src/components/room/RoomVideoController.vue
+++ src/components/room/RoomVideoController.vue
@@ -1,29 +1,49 @@
 <template>
-	<RoomVideo
-		ref="video"
-		:room="room"
-		:controls="isHost"
-		:hide-controls="!allowControls"
-	/>
+	<div id="room-video-and-chat-container">
+		<div v-if="show && episode">
+			<Video
+
+				ref="video"
+				:show="show"
+				:episode="episode"
+				:controls="isHost"
+				:hide-controls="!allowControls"
+
+				@update="pushSync"
+			/>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 
 	// Modules
-	import { defineComponent } from "vue";
+	import { defineComponent, ref } from "vue";
 
 	// Components
-	import RoomVideo from "@components/room/RoomVideo.vue";
+	import Video from "@components/Video.vue";
 
 	// Mixins
 	import RoomMixin from "@mixins/Room";
 
+	// Types
+	import { RoomSyncData } from "@typings/room";
+	import { SocketResponse } from "@typings/main";
+
 	export default defineComponent({
 		name: "RoomVideoController",
 		components: {
-			RoomVideo
+			Video
 		},
 		mixins: [ RoomMixin ],
+		setup () {
+
+			const video = ref<HTMLVideoElement>();
+
+			return {
+				video
+			};
+		},
 		data () {
 			return {
 				teleportParent: "room-video-container" as string | null
@@ -55,12 +75,27 @@
 					const
 						newParentInstance = document.getElementById(this.teleportParent),
 						// eslint-disable-next-line
-						videoRef = (this.$refs.video as any)?.$el as HTMLElement; // I deserve to get waterboarded for this
+						videoRef = (this.video as any)?.$el as HTMLElement; // I deserve to get waterboarded for this
 
 					if (newParentInstance && videoRef) {
 						newParentInstance.innerHTML = "";
 						newParentInstance.appendChild(videoRef);
 					}
+				}
+			},
+			pushSync (playing: boolean, currentTime: number) {
+				if (this.isHost) {
+
+					const payload: RoomSyncData = {
+						playing: playing,
+						currentTime: currentTime
+					};
+
+					this.$socket.emit("CLIENT:SYNC_ROOM", payload, (res: SocketResponse<string>) => {
+						if (res.type !== "success") {
+							console.error(res.message);
+						}
+					});
 				}
 			}
 		}

77b1a598e2e3b7a75c92da3b514c0d61656784e0
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-02 14:39:48 +0200

    improve getEpisodeById

--- src/utils/show.ts
+++ src/utils/show.ts
@@ -2,16 +2,20 @@
 import { Episode, Show } from "@typings/show";
 
 export function getEpisodeById (show: Show, episodeId: number): Episode | null {
-	
+
 	let _episode: Episode | null = null;
 
-	show.seasons.forEach(({ episodes }) => {
-		episodes.forEach((episode: Episode) => {
+	for (const { episodes } of show.seasons) {
+
+		if (_episode) break;
+
+		for (const episode of episodes) {
 			if (episode.id === episodeId) {
 				_episode = episode;
+				break;
 			}
-		});
-	});
+		}
+	}
 
 	return _episode;
 }

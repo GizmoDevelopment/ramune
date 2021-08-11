30f9c5b386fdf72b7da21b2d2e9c95982982513f
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-11 19:49:28 +0200

    joinRoom method in mixin

---
+++ src/mixins/RoomMethods.ts
@@ -0,0 +1,33 @@
+// Modules
+import { defineComponent } from "vue";
+
+// Types
+import { SocketResponse } from "@typings/main";
+import { JoinRoomOptions, Room } from "@typings/room";
+
+export default defineComponent({
+	methods: {
+		joinRoom (options: JoinRoomOptions): Promise<Room> {
+			return new Promise((res, rej) => {
+				if (this.$store.state.room.room?.id === options.id) {
+					this.$router.push(`/rooms/${options.id}`);
+				} else {
+					this.$socket.client.emit("CLIENT:JOIN_ROOM", options, (response: SocketResponse<Room>) => {
+						if (response.type === "success") {
+
+							const room = response.data;
+
+							this.$store.commit("room/JOIN_ROOM", room);
+							this.$router.push(`/rooms/${room.id}`);
+
+							res(room);
+						} else {
+							console.error(response.message);
+							rej(response.message);
+						}
+					});
+				}
+			});
+		}
+	}
+});

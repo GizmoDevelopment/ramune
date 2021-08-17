eeac877ff749aac40857c15d6c4ab480884dd473
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-17 14:08:13 +0200

    split global styles

--- src/views/App.vue
+++ src/views/App.vue
@@ -42,7 +42,6 @@
 	// Types
 	import { AuthenticatedUser, User } from "gizmo-api/lib/types";
 	import { Room, RoomData } from "@typings/room";
-	import { Theme } from "@typings/main";
 
 	export default defineComponent({
 		name: "App",
@@ -60,9 +59,6 @@
 		computed: {
 			isViewingRoom (): boolean {
 				return this.$route.path.match(/^\/rooms\/.+$/i) !== null;
-			},
-			theme (): Theme {
-				return this.$store.getters["settings/theme"];
 			}
 		},
 		watch: {
@@ -166,7 +162,11 @@
 <style lang="scss">
 
 	@import "@styles/main.scss";
+	@import "@styles/global.scss";
+	@import "@styles/font.scss";
+	@import "@styles/animations.scss";
 	@import "@styles/mixins.scss";
+	@import "@styles/tooltip.scss";
 
 	:root {
 
@@ -175,11 +175,11 @@
 		/* Colors */
 		--text-color: white;
 		--faded-text-color: #868686;
+		--primary-color: #3db8f5;
+		--primary-hover-color: #0a85c2;
 		--container-background-color: rgb(31, 31, 31);
 		--container-hover-color: #494949;
 		--background-color: #0D0D0D;
-		--primary-color: v-bind(theme.primary);
-		--primary-hover-color: v-bind(theme.primaryHover);
 		--transparent-hover-color: rgba(13, 13, 13, .5);
 		--error-color: hsl(0, 95%, 60%);
 
@@ -192,60 +192,6 @@
 		--easing-enter: cubic-bezier(0, 1.68, .43, 1.01);
 		--easing-leave: cubic-bezier(.67, 0, 1, -.83);
 
-	}
-
-	/* Mixins */
-
-	@mixin light () {
-		background-color: var(--container-background-color);
-		border-color: var(--container-background-color);
-	}
-
-	@mixin dark () {
-		background-color: var(--background-color);
-		border-color: var(--background-color);
-	}
-
-	/* Fonts */
-
-	@font-face {
-		font-family: "Roboto";
-		src: url("../assets/fonts/Roboto-Bold.ttf");
-		font-weight: 700;
-		font-style: normal;
-		font-display: swap;
-	}
-
-	@font-face {
-		font-family: "Roboto";
-		src: url("../assets/fonts/Roboto-Light.ttf");
-		font-weight: 300;
-		font-style: normal;
-		font-display: swap;
-	}
-
-	@font-face {
-		font-family: "Roboto";
-		src: url("../assets/fonts/Roboto-Medium.ttf");
-		font-weight: 500;
-		font-style: normal;
-		font-display: swap;
-	}
-
-	@font-face {
-		font-family: "Roboto";
-		src: url("../assets/fonts/Roboto-Regular.ttf");
-		font-weight: 400;
-		font-style: normal;
-		font-display: swap;
-	}
-
-	@font-face {
-		font-family: "Roboto";
-		src: url("../assets/fonts/Roboto-Thin.ttf");
-		font-weight: 100;
-		font-style: normal;
-		font-display: swap;
 	}
 
 	/* Transitions */
@@ -285,243 +231,14 @@
 		opacity: 0;
 	}
 
-	/* Animations */
-
-	@keyframes spinning {
-		0% {
-			transform: rotate(0turn);
-		}
-		100% {
-			transform: rotate(1turn);
-		}
-	}
-
-	@keyframes gradient {
-		0% {
-			background-position: left;
-		}
-		65% {
-			background-position: right;
-		}
-		100% {
-			background-position: left;
-		}
-	}
-
-	/* Classes */
-
-	.primary-button, .secondary-button {
-		color: var(--text-color);
-		border: 0;
-		border-radius: 10px;
-		padding: 5px;
-		padding-left: 20px;
-		padding-right: 20px;
-		font-size: 18px;
-		font-weight: 500;
-		transition: .2s background-color ease-in-out, .2s opacity ease;
-		cursor: pointer;
-	}
-
-	.primary-button {
-
-		background-color: var(--primary-color);
-		border-color: var(--primary-color);
-
-		&:hover {
-			background-color: var(--primary-hover-color);
-		}
-	}
-
-	.secondary-button {
-
-		@include light;
-
-		&:hover {
-			background-color: var(--container-hover-color);
-		}
-
-		&[theme=dark]:hover {
-			background-color: var(--container-background-color);
-		}
-
-	}
-
-	.icon-button {
-		border-width: 2px;
-		padding: 6px;
-		font-size: 1.5em;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		align-content: center;
-	}
-
-	.disabled-button {
-		opacity: .5;
-		cursor: default;
-	}
-
-	.input {
-
-		@include light;
-
-		font-family: var(--font);
-		color: var(--text-color);
-		border: 2px solid transparent;
-		border-radius: var(--popup-border-radius);
-		font-size: 1.1em;
-		outline: none;
-		padding: .2em .4em .2em .4em;
-		box-sizing: border-box;
-		transition: border-color .2s ease-in-out;
-
-		&:focus {
-			border-color: var(--primary-color);
-		}
-	}
-
-	.heading {
-
-		@include text-wrap;
-
-		// Defaults
-		font-size: 2.8rem;
-		margin-top: 1rem;
-		margin-bottom: 1rem;
-
-		@at-root h1#{&} {
-			font-size: 2.8rem;
-			margin-top: 1rem;
-			margin-bottom: 1rem;
-		}
-
-		@at-root h2#{&} {
-			font-size: 2.2rem;
-			margin-top: .8rem;
-			margin-bottom: .8rem;
-		}
-
-		@at-root h3#{&} {
-			font-size: 1.5rem;
-			margin-top: .4rem;
-			margin-bottom: .4rem;
-		}
-
-		font-weight: bold;
-		text-align: left;
-	}
-
-	@media screen and (max-width: 700px) {
-
-		.heading {
-
-			@at-root h1#{&} {
-				font-size: 2.3rem;
-			}
-
-			@at-root h2#{&} {
-				font-size: 1.9rem;
-			}
-
-			@at-root h3#{&} {
-				font-size: 1.2rem;
-			}
-
-		}
-	}
-
-	.faded {
-		color: var(--faded-text-color);
-	}
-
-	.error {
-		color: var(--error-color);
-		margin-top: 0;
-		margin-bottom: 0;
-	}
-
-	.icon {
-		height: 1em;
-	}
-
-	.icon path {
-		fill: var(--text-color);
-		stroke: var(--text-color);
-	}
-
-	.no-fill path {
-		fill: none !important;
-	}
-
-	.husk, .husk-dark {
-		color: transparent;
-		border-radius: var(--card-border-radius);
-		-ms-user-select: none;
-		-webkit-user-select: none;
-		user-select: none;
-		animation: gradient 5s ease infinite;
-	}
-
-	.husk-dark {
-		background: linear-gradient(45deg, var(--background-color), #181818);
-		background-size: 200% 100%;
-	}
-
-	.husk {
-		background: linear-gradient(45deg, var(--container-background-color), #111);
-		background-size: 200% 100%;
-	}
-
-	.overlay {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		left: 0;
-		top: 0;
-		background-color: var(--overlay-background-color);
-		border-radius: var(--card-border-radius);
-		transition: opacity .2s ease;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.hover-opacity {
-		opacity: 0;
-		transition: opacity .2s ease-in-out;
-	}
-
-	.hover-opacity:hover {
-		opacity: 1;
-		cursor: pointer;
-	}
-
-	.anchor-without-decoration {
-		text-decoration: none;
-		color: inherit;
-	}
-
-	.hover-container {
-
-		transition: .2s background ease-out;
-		border-radius: var(--card-border-radius);
-
-		&:hover {
-			background-color: var(--transparent-hover-color);
-			cursor: pointer;
-		}
-	}
 
 	/* Globals */
 
-	*[theme=light] {
+	*[variant=light] {
 		@include light;
 	}
 
-	*[theme=dark] {
+	*[variant=dark] {
 		@include dark;
 	}
 
@@ -655,11 +372,5 @@
 		margin-left: 15px;
 		margin-bottom: 15px;
 	}
-
-</style>
-
-<style lang="scss">
-
-	@import "@styles/tooltip.scss";
 
 </style>

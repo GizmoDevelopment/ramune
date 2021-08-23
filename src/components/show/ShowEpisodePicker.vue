9282abac98a861f48cc0ffbcb4b4d6f817a91197
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-23 15:51:25 +0200

    episode picker

---
+++ src/components/show/ShowEpisodePicker.vue
@@ -0,0 +1,144 @@
+<template>
+	<div class="episode-picker-container">
+		<div v-if="show.seasons.length > 1" class="title-bar">
+			<Dropdown
+				:entries="seasonList"
+				:current-index="currentSeasonIndex"
+				:variant="variant"
+				@select-index="selectSeason"
+			/>
+		</div>
+		<div class="episode-list">
+			<ShowEpisodeCard
+				v-for="(episode, index) in currentSeason.episodes"
+				:id="index + 1"
+				:key="episode.id"
+				v-memo="[ episode.id === currentEpisodeId ]"
+				:episode="episode"
+				:active="episode.id === currentEpisodeId"
+				@select-episode="selectEpisode"
+			/>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent, PropType } from "vue";
+
+	// Components
+	import Dropdown from "@components/Dropdown.vue";
+	import ShowEpisodeCard from "@components/show/ShowEpisodeCard.vue";
+
+	// Mixins
+	import MainMixin from "@mixins/Main";
+	import GenericMixin from "@mixins/Generic";
+
+	// Types
+	import { Season, Show } from "@typings/show";
+	import { SocketResponse } from "@typings/main";
+	import { Room } from "@typings/room";
+
+	export default defineComponent({
+		name: "ShowEpisodePicker",
+		components: {
+			Dropdown,
+			ShowEpisodeCard
+		},
+		mixins: [ MainMixin, GenericMixin ],
+		props: {
+			show: {
+				type: Object as PropType<Show>,
+				required: true
+			},
+			currentEpisodeId: {
+				type: Number,
+				default: 0
+			}
+		},
+		data () {
+			return {
+				currentSeasonIndex: 0,
+				seasonList: [] as string[]
+			};
+		},
+		computed: {
+			isHost (): boolean {
+				return this.room?.host?.id === this.user?.id;
+			},
+			currentSeason (): Season {
+				return this.show.seasons[this.currentSeasonIndex];
+			}
+		},
+		watch: {
+			show: {
+				immediate: true,
+				handler () {
+					this.currentSeasonIndex = 0;
+					this.seasonList = this.show.seasons.map(season => `Season ${season.id}`);
+				}
+			}
+		},
+		methods: {
+			selectEpisode (episodeId: number) {
+				if (this.room && this.user && this.isHost) {
+
+					this.$store.commit("room/UPDATE_ROOM_DATA_LOADING_STATE", true);
+
+					this.$socket.client.emit("CLIENT:UPDATE_ROOM_DATA", { showId: this.show.id, episodeId }, (res: SocketResponse<Room>) => {
+						if (res.type !== "success") {
+							console.error(res.message);
+						}
+					});
+
+					// Don't make the user wait to transition back to the room
+					this.$router.push(`/rooms/${this.room?.id}`);
+
+				} else {
+
+					if (this.room) {
+						this.$socket.client.emit("CLIENT:LEAVE_ROOM", this.room.id, (res: SocketResponse<Room>) => {
+							if (res.type === "success") {
+								this.$store.commit("room/LEAVE_ROOM");
+							} else {
+								console.error(res.message);
+							}
+						});
+					}
+
+					this.$router.push(`/watch/${this.show.id}/${episodeId}`);
+				}
+			},
+			selectSeason (seasonIndex: number) {
+				this.currentSeasonIndex = seasonIndex;
+			}
+		}
+	});
+
+</script>
+
+<style scoped lang="scss">
+
+	@import "@styles/main.scss";
+
+	.episode-picker-container {
+		margin-top: 1rem;
+	}
+
+	.title-bar {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		margin-bottom: 1rem;
+	}
+
+	.episode-list {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: flex-start;
+		flex-wrap: wrap;
+	}
+
+</style>

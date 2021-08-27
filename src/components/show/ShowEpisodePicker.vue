f761662bbc27ca5910f8d18299d31f1da8c82aed
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-27 14:23:36 +0200

    fix episode picker not updating

--- src/components/show/ShowEpisodePicker.vue
+++ src/components/show/ShowEpisodePicker.vue
@@ -15,7 +15,6 @@
 				v-for="(episode, index) in currentSeason.episodes"
 				:id="index + 1"
 				:key="`${show.id}-${episode.id}`"
-				v-memo="[ episode.id === currentEpisodeId ]"
 				:episode="episode"
 				:active="episode.id === currentEpisodeId"
 				@select-episode="selectEpisode"

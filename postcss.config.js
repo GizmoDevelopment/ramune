13f3cdef6f47e8e9b9b11697867680f5c2cac825
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-10-01 19:10:26 +0200

    postcss-calc

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
 module.exports = {
 	plugins: [
-		require("autoprefixer"),
 		require("stylelint"),
 		require("postcss-reporter"),
+		require("postcss-calc"),
+		require("autoprefixer"),
 		require("@fullhuman/postcss-purgecss")({
 			content: [
 				"index.html",

71d77241acd6340dca6681bb5a2d812626a1c62a
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-10-07 18:35:45 +0200

    remove flavor computed from mixin

--- src/mixins/Generic.ts
+++ src/mixins/Generic.ts
@@ -1,19 +1,11 @@
 // Modules
 import { defineComponent } from "vue";
 
-// Types
-import { Flavor } from "@typings/main";
-
 export default defineComponent({
 	props: {
 		variant: {
 			type: String,
 			default: "light"
-		}
-	},
-	computed: {
-		flavor (): Flavor {
-			return this.$store.getters["settings/flavor"];
 		}
 	}
 });

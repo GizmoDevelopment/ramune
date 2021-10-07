280590133238ad7873c39f9db7da45b864d41808
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-10-07 17:27:00 +0200

    SSR

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,54 @@
-// Factories
-import { createApp } from "@factories/app";
+// Modules
+import viteSSR, { ClientOnly } from "vite-ssr";
+import VueSocketIO from "vue-socket.io-extended";
+import { io } from "socket.io-client";
+import { createHead } from "@vueuse/head";
 
-createApp().mount("#app");
+// Factories
+import { createRouter } from "@factories/router";
+import { createStore } from "@factories/store";
+
+// Directives
+import Tooltip from "@directives/tooltip";
+
+// Views
+import App from "@views/App.vue";
+
+// Constants
+const SOCKET_ENDPOINT = import.meta.env.VITE_SOCKET_ENDPOINT;
+
+// Variables
+const router = createRouter();
+const ioInstance = io(SOCKET_ENDPOINT);
+
+if (typeof SOCKET_ENDPOINT !== "string") {
+	throw Error("Environmental variable 'SOCKET_ENDPOINT' is not assigned");
+}
+
+export default viteSSR (App, { routes: router.options.routes }, ({ app, router, initialState }) => {
+
+	const
+		head = createHead(),
+		store = createStore();
+
+	app
+		.use(createHead)
+		.use(store)
+		.use(VueSocketIO, ioInstance)
+		.directive("tooltip", Tooltip)
+		.component(ClientOnly.name, ClientOnly);
+
+	// Hydrate app with initialState that is passed to VueX
+	app.provide("initialState", initialState);
+
+	router.beforeEach(async (to: any, _: unknown, next: any) => {
+
+		if (to.meta.state) {
+			return next();
+		}
+
+		next();
+	});
+
+	return { head };
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import svgLoader from "vite-svg-loader";
+import viteSSR from "vite-ssr/plugin";
 
 export default defineConfig({
 	plugins: [
+		viteSSR(),
 		vue(),
 		svgLoader()
 	],

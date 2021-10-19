21bf3b31e28ef89e10ecbd791b00227067493560
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-10-19 13:25:57 +0200

    reorganize dropdown style

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,19 +1,19 @@
 <template>
 	<div
 		class="dropdown"
-		:class="{ 'dropdown-active': isOpen }"
+		:class="{ 'active': isOpen }"
 		:variant="variant"
 		@click="isOpen = !isOpen"
 		@mouseleave="isOpen = isOpen ? !isOpen : isOpen"
 	>
 		<div class="dropdown-currently-selected">
 			<p class="dropdown-currently-selected-label">{{ entries[currentIndex] }}</p>
-			<Caret class="dropdown-icon" :class="{ 'dropdown-icon-active': isOpen }" />
+			<Caret class="dropdown-icon" :class="{ 'active': isOpen }" />
 		</div>
 		<transition name="dropdown">
 			<div v-show="isOpen" class="dropdown-holder">
 				<div
-					class="dropdown-container dropdown-container-active"
+					class="dropdown-container"
 					:variant="variant"
 				>
 					<button
@@ -22,7 +22,7 @@
 						v-memo="[ currentIndex === index ]"
 						class="secondary-button dropdown-entry"
 						:variant="variant"
-						:class="{ 'dropdown-entry-active': currentIndex === index }"
+						:class="{ 'active': currentIndex === index }"
 						@click="$emit('select-index', index)"
 					>
 						{{ entry }}
@@ -95,19 +95,26 @@
 	//
 
 	.dropdown {
+
 		display: inline-block;
 		position: relative;
 		cursor: pointer;
 		padding: .4rem .7rem .4rem .7rem;
+		border-radius: variable(popup-border-radius);
 		border-top: $empty-border;
-	}
 
-	.dropdown-holder {
-		position: absolute;
-		width: calc(100% + 4px); // ? Because border-box doesn't account for the border...?
-		top: 100%;
-		left: -2px; // ? Because border-box doesn't account for the border...?
-		z-index: 2;
+		&.active {
+			border-radius: variable(popup-border-radius) variable(popup-border-radius) 0 0;
+			border-top: $filled-border;
+		}
+
+		.dropdown-holder {
+			position: absolute;
+			width: calc(100% + 4px); // ? Because border-box doesn't account for the border...?
+			top: 100%;
+			left: -2px; // ? Because border-box doesn't account for the border...?
+			z-index: 2;
+		}
 	}
 
 	.dropdown-container {
@@ -119,31 +126,19 @@
 		flex-direction: column;
 		justify-content: flex-start;
 		align-items: flex-start;
-		border-top: none;
-		border-bottom: $empty-border;
 		border-radius: 0 0 variable(popup-border-radius) variable(popup-border-radius);
+		border-bottom: $filled-border;
 	}
 
 	.dropdown,
 	.dropdown-container {
-		border-radius: variable(popup-border-radius);
 		border-left: $empty-border;
 		border-right: $empty-border;
 		transition: border $border-transition, border-radius $border-transition;
 	}
 
-	.dropdown-active {
-		border-radius: variable(popup-border-radius) variable(popup-border-radius) 0 0;
-		border-top: $filled-border;
-	}
-
-	.dropdown-container-active {
-		border-radius: 0 0 variable(popup-border-radius) variable(popup-border-radius);
-		border-bottom: $filled-border;
-	}
-
-	.dropdown-active,
-	.dropdown-container-active {
+	.dropdown.active,
+	.dropdown-container {
 		border-left: $filled-border;
 		border-right: $filled-border;
 	}
@@ -161,18 +156,20 @@
 			font-size: 1.4em;
 			user-select: none;
 		}
-	}
 
-	.dropdown-icon {
-		transition: transform .3s ease;
-		transform: rotateZ(90deg);
-	}
+		.dropdown-icon {
 
-	.dropdown-icon-active {
-		transform: rotateZ(90deg) rotateY(180deg);
+			transition: transform .3s ease;
+			transform: rotateZ(90deg);
+
+			&.active {
+				transform: rotateZ(90deg) rotateY(180deg);
+			}
+		}
 	}
 
 	.dropdown-entry {
+
 		width: 100%;
 		padding: .3rem .7rem .3rem .7rem;
 		font-size: 1.2rem;
@@ -180,10 +177,10 @@
 		user-select: none;
 		text-align: left;
 		transition: background-color .3s ease;
-	}
 
-	.dropdown-entry-active {
-		background-color: variable(container-hover-color);
+		&.active {
+			background-color: variable(container-hover-color);
+		}
 	}
 
 </style>

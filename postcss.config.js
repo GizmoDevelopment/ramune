534110e813265df55c9387e800db184a150fe1ba
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-10-22 14:38:37 +0200

    disable some stylelint rules on PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 /* eslint-disable no-undef */
 module.exports = {
 	plugins: [
-		require("stylelint"),
+		// Fixes errors with Vite's bundled CSS
+		require("stylelint")({
+			rules: {
+				indentation: null,
+				"no-missing-end-of-source-newline": null,
+				"number-leading-zero": null
+			}
+		}),
 		require("postcss-reporter"),
 		require("postcss-calc"),
 		require("autoprefixer"),

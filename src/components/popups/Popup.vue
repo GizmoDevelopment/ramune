23e5c18c53f1edf742756c8b605c7e433f735856
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-10-22 16:21:32 +0200

    re-write popups

--- src/components/popups/Popup.vue
+++ src/components/popups/Popup.vue
@@ -1,40 +1,46 @@
 <template>
 	<teleport to="#app">
-		<transition name="fade-popup-overlay">
+		<div class="popup">
+			<transition name="fade-popup-overlay">
+				<div
+					v-if="visible"
+					class="overlay"
+				/>
+			</transition>
 			<div
-				v-if="visible"
-				class="overlay"
-				@click="dismiss"
-			/>
-		</transition>
-		<transition name="slide-popup">
-			<div
-				v-if="visible"
-				class="popup-positioner"
+				class="popup-scroller"
+				:style="{
+					pointerEvents: scrollerPointerEvents,
+					overflowY: scrollerOverflowY
+				}"
 				@click="dismiss"
 			>
-				<div
-					class="popup-container"
-					:class="{ 'transparent-popup': transparent }"
-					:style="alignment"
-					@click.stop
-				>
-					<div
-						v-if="title || dismissable"
-						class="popup-title-bar"
-						:class="{ 'floating-title-bar': floatingTitleBar }"
-					>
-						<h2 class="popup-title">{{ title }}</h2>
-						<Close
-							v-if="dismissable"
-							class="popup-close-button"
-							@click="dismiss"
-						/>
-					</div>
-					<slot />
+				<div class="popup-container">
+					<transition name="slide-popup">
+						<div
+							v-if="visible"
+							class="popup-content"
+							:class="{ 'transparent-popup': transparent }"
+							@click.stop
+						>
+							<div
+								v-if="title || dismissable"
+								class="popup-title-bar"
+								:class="{ 'floating-title-bar': floatingTitleBar }"
+							>
+								<h2 class="popup-title">{{ title }}</h2>
+								<Close
+									v-if="dismissable"
+									class="popup-close-button"
+									@click="dismiss"
+								/>
+							</div>
+							<slot />
+						</div>
+					</transition>
 				</div>
 			</div>
-		</transition>
+		</div>
 	</teleport>
 </template>
 
@@ -60,15 +66,6 @@
 				type: Boolean,
 				default: false
 			},
-			align: {
-				type: String,
-				default: "center",
-				validator: (prop: string) => [
-					"left",
-					"center",
-					"right"
-				].includes(prop)
-			},
 			dismissable: {
 				type: Boolean,
 				default: true
@@ -83,9 +80,38 @@
 			}
 		},
 		emits: [ "dismiss" ],
+		data () {
+			return {
+				initial: true
+			};
+		},
 		computed: {
-			alignment (): string {
-				return `{ align-items: ${this.align}; }`;
+			scrollerPointerEvents () {
+				return this.visible
+					? "auto"
+					: "none";
+			},
+			scrollerOverflowY () {
+				return this.visible
+					? "scroll"
+					: "hidden";
+			}
+		},
+		watch: {
+			visible: {
+				immediate: true,
+				handler (state: boolean) {
+
+					const body = document.getElementsByTagName("body");
+
+					if (state) {
+						body[0].classList.add("no-scroll");
+					} else if (!this.initial) {
+						body[0].classList.remove("no-scroll");
+					}
+
+					this.initial = false;
+				}
 			}
 		},
 		methods: {
@@ -101,7 +127,7 @@
 
 <style scoped lang="scss">
 
-	@import "@styles/main.scss";
+	@import "@styles/main";
 
 	// Transitions
 
@@ -116,57 +142,75 @@
 	}
 
 	.slide-popup-enter-active {
-		transition: transform .45s variable(easing-enter), opacity .25s ease
+		transition: transform .45s variable(easing-enter), opacity .25s ease;
 	}
 
 	.slide-popup-leave-active {
-		transition: transform .3s variable(easing-leave), opacity .25s ease
+		transition: transform .3s ease-out, opacity .25s ease;
 	}
 
 	.slide-popup-enter-from,
 	.slide-popup-leave-to {
-		transform: translateY(4rem) scale(98%);
 		opacity: 0;
+	}
+
+	.slide-popup-enter-from {
+		transform: translateY(3rem) scale(99%);
+	}
+
+	.slide-popup-leave-to {
+		transform: translateY(1rem) scale(99%);
 	}
 
 	//
 
-	.overlay,
-	.popup-positioner {
+	.popup {
+
 		position: fixed;
+
 		z-index: 20;
-	}
+		pointer-events: none;
 
-	.popup-positioner {
-		width: 100%;
-		height: 100%;
-		top: 0;
-		left: 0;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: flex-start;
-		overflow-y: auto;
-	}
+		.overlay {
+			position: fixed;
+		}
 
-	.popup-container {
-		position: relative;
-		margin-top: 5%;
-		margin-bottom: 20px;
-		width: auto;
-		min-width: 300px;
-		background-color: variable(container-background-color);
-		border-radius: variable(popup-border-radius);
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-start;
-		align-items: center;
-		padding-top: 1em;
-		padding-bottom: 1em;
-		padding-left: 1em;
-		padding-right: 1em;
-		box-shadow: 0px 10px 30px 5px rgba(0, 0, 0, .6);
-		overflow-x: hidden;
+		.popup-scroller {
+
+			position: fixed;
+			width: 100%;
+			height: 100vh;
+			top: 0;
+
+			display: flex;
+			justify-content: center;
+
+			pointer-events: auto;
+			overflow-x: hidden;
+			overflow-y: scroll;
+
+			.popup-container {
+				.popup-content {
+
+					position: relative;
+					width: auto;
+					min-width: 300px;
+
+					margin: 10% 0 20px 0;
+					padding: 1em;
+					box-shadow: 0 10px 30px 5px rgba(0, 0, 0, .6);
+
+					background-color: variable(container-background-color);
+					border-radius: variable(popup-border-radius);
+
+					display: flex;
+					flex-direction: column;
+					justify-content: flex-start;
+					align-items: center;
+				}
+			}
+		}
+
 	}
 
 	.popup-title-bar {
@@ -199,15 +243,15 @@
 		left: 1em;
 	}
 
-	@media only screen and (max-width: 1250px) {
-		.popup-container {
-			width: 90%;
-		}
-	}
-
 	.transparent-popup {
 		background-color: transparent !important;
 		box-shadow: none !important;
+	}
+
+	@media only screen and (max-width: 1250px) {
+		.popup-content {
+			width: 90%;
+		}
 	}
 
 </style>

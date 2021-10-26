5c484250e93dee860015323f8e8a94bc229013a5
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-10-26 14:24:43 +0200

    JSON-ify settings

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,22 +1,13 @@
-// ! IF YOU'RE ADDING SAVABLE OBJECTS/ARRAYS/MAPS, MAKE SURE TO STRINGIFY THEM PROPERLY
-
 export function saveSetting (name: string, value: unknown): void {
-	window.localStorage.setItem(name, `${value}`);
+	window.localStorage.setItem(name, JSON.stringify(value));
 }
 
-export function getSetting <T> (name: string, defaultValue: T): unknown {
+export function getSetting <T> (name: string, defaultValue: T): T {
 
 	const setting = window.localStorage.getItem(name);
 
 	if (setting !== null) {
-		switch (typeof defaultValue) {
-			case "number":
-				return Number(setting);
-			case "boolean":
-				return setting === "true" ? true : false;
-			default: // Strings
-				return setting;
-		}
+		return JSON.parse(setting);
 	} else {
 		return defaultValue;
 	}

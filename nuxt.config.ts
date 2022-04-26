5a63fd04783e127c3bbaa1274bd1beb4b0fe7e5e
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-04-26 22:23:21 +0200

    import dev tool configs into nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,16 @@ import { defineNuxtConfig } from "nuxt";
 
 export default defineNuxtConfig({
 	srcDir: "src/",
+	modules: [
+		"@nuxtjs/stylelint-module"
+	],
 	typescript: {
 		shim: false
+	},
+	postcss: {
+		config: true
+	},
+	vite: {
+		configFile: "vite.config.ts"
 	}
 });

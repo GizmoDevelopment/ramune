cee4e135ba0957d60f9374c6d2eb4b750e07dae8
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-07-16 18:36:12 +0200

    remove SVG loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 // Modules
 import { defineConfig } from "vite";
-import svgLoader from "vite-svg-loader";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-	plugins: [
-		svgLoader()
-	],
 	build: {
 		rollupOptions: {
 			plugins: [

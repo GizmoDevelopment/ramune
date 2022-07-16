788126c2e0da5b18d84be6ba49b67d60a00da602
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-07-16 19:41:40 +0200

    add doc links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // Modules
 import { defineNuxtConfig } from "nuxt";
 
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	modules: [
 		"@nuxtjs/stylelint-module"
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
 import eslintPlugin from "vite-plugin-eslint";
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		eslintPlugin()

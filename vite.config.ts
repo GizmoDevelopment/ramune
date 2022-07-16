40f319ea36f406ca0548fd9a4a3cfcbaabd6a936
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-07-16 18:38:50 +0200

    `vite-plugin-eslint`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 // Modules
 import { defineConfig } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
+	plugins: [
+		eslintPlugin()
+	],
 	build: {
 		rollupOptions: {
 			plugins: [

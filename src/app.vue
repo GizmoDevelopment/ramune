a813e4fa103e6528cc8c6643077e4f50a3d82ff6
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-07-16 20:08:36 +0200

    app component

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,18 @@
 <template>
-	<h1>Hello!</h1>
+	<h1>Ramune</h1>
 </template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "App"
+	});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
